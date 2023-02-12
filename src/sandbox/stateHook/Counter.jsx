1d8f60c0839c4772d5e5917ac4675abe4ff19da5
Dima <pushokf@gmail.com>
2023-02-12 20:39:14 +0200

    stateHook practice

---
+++ src/sandbox/stateHook/Counter.jsx
@@ -0,0 +1,35 @@
+import { Button } from "@mui/material";
+import React, { useState } from "react";
+
+export default function Counter() {
+    ////destructuring reminder
+    // let arr = [1, 2, 3, 4];
+    // let obj = { firstName: "Tzach", lastName: "Dabush" };
+    // let [first, second, third, forth] = arr;
+    // let { firstName, lastName } = obj;
+    // console.log(firstName);
+
+    const [counter, setCounter] = useState(0);
+
+    return (
+        <>
+            <p>The number is {counter}</p>
+            <Button
+                variant="outlined"
+                onClick={() => {
+                    setCounter(counter + 1);
+                }}
+            >
+                Increment +
+            </Button>
+            <Button
+                variant="outlined"
+                onClick={() => {
+                    setCounter(counter - 1);
+                }}
+            >
+                Decrement -
+            </Button>
+        </>
+    );
+}
